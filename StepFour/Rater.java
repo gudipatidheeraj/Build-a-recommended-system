bf0a42c1810fa50f8a054559e3bd237e65f2f41e
Gudipati Dheeraj <47204287+gudipatidheeraj@users.noreply.github.com>
2020-06-07 17:29:36 +0530

    Add files via upload

---
+++ StepFour/Rater.java
@@ -0,0 +1,23 @@
+
+/**
+ * Rater is an interface for PlainRater and EfficientRater classes.
+ * 
+ * @ Gudipati Dheeraj
+ * @ 5/6/2020
+ */
+
+import java.util.*;
+
+public interface Rater {
+    public void addRating(String item, double rating);
+
+    public boolean hasRating(String item);
+
+    public String getID();
+
+    public double getRating(String item);
+
+    public int numRatings();
+
+    public ArrayList<String> getItemsRated();
+}
